c5e0d1c061136e97ac4dc9c9af6b94529382f788
hesham <17p6020@eng.asu.edu.eg>
2021-01-30 04:49:20 +0200

    initial

---
+++ android/app/src/main/java/com/hesham/buttons/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hesham.buttons;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:buttons/Screens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
